9eded5b5ec61ab15591b2e4d617aef8fe9f02202
Ryan Daigle <ryan.daigle@gmail.com>
2011-06-27 13:49:56 -0400

    Comment out index tank indexing for now

--- models/tweet.rb
+++ models/tweet.rb
@@ -2,8 +2,8 @@ load "config/active_record.rb"
 load "config/index_tank.rb"
 
 class Tweet < ActiveRecord::Base
-  after_create { |tweet| 
-    puts "Indexing #{tweet["text"]}"
-    $index.document(tweet.to_param).add(:text => tweet.text, :user => tweet.user)
-  }
+#  after_create { |tweet| 
+#    puts "Indexing #{tweet["text"]}"
+#    $index.document(tweet.to_param).add(:text => tweet.text, :user => tweet.user)
+#  }
 end
